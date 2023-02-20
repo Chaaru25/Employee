9fced57d43edda4bd19f7fdb0ff7b28289254371
Sarumathy A <sarumathy.a@csoft-tech.com>
2023-02-20 22:57:58 +0530

    commi the crud

---
+++ app/controllers/employees_controller.rb
@@ -0,0 +1,39 @@
+class EmployeesController < ApplicationController
+  def index
+    @employees = EmployeeDetail.all;
+  end
+
+  def show
+    @employee = EmployeeDetail.find(params[:id])
+  end
+
+  def new
+    @employee = EmployeeDetail.new
+  end
+
+  def create
+    employee = EmployeeDetail.create(employee_params)
+    redirect_to employee_path
+  end
+   def edit
+    @employee = EmployeeDetail.find(params[:id])
+ end
+  def update
+    @employee = EmployeeDetail.find(params[:id]) 
+    @employee = update(employee_params)
+    redirect_to employee_path
+  end
+  def destroy
+    @employee = EmployeeDetail.find(params[:id]) 
+    @employee = destroy
+    redirect_to employee_path
+
+
+  end
+ private
+ def employee_params
+  params.require(:employeeDetail).permit(:name,:age,:gender,:designation)
+ end
+
+ 
+end
